39d30cf2bbee23e3fa02946e97278c1fb9201512
Wurmatron <wurmatron@gmail.com>
2019-05-05 20:20:19 -0400

    Added Module Recipes Fixed #10 Version Bump

--- src/main/java/com/wurmatron/mininggoggles/MiningGoggles.java
+++ src/main/java/com/wurmatron/mininggoggles/MiningGoggles.java
@@ -11,7 +11,10 @@ import com.wurmatron.mininggoggles.common.registry.ModuleRegistry;
 import com.wurmatron.mininggoggles.common.registry.Registry;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -22,6 +25,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Global.MODID, name = Global.NAME, version = Global.VERSION, dependencies = Global.DEPENDENCIES)
@@ -51,29 +55,86 @@ public class MiningGoggles {
   public void onInit(FMLInitializationEvent e) {
     proxy.init();
     try {
+      ResourceLocation RECIPE_GROUP = new ResourceLocation(Global.MODID, "Recipes");
+      // Goggles
       GameRegistry.addShapedRecipe(new ResourceLocation(Global.MODID, "miningGoggles4"),
-          new ResourceLocation(Global.MODID, "Recipes"),
+          RECIPE_GROUP,
           ItemGogglesMining.create(4, new IModule[0]), "LGL", "IXI", 'L', Items.LEATHER, 'G',
           "ingotGold", 'I', "ingotIron", 'X',
           Items.ENDER_PEARL);
       GameRegistry.addShapedRecipe(new ResourceLocation(Global.MODID, "miningGoggles8"),
-          new ResourceLocation(Global.MODID, "Recipes"),
+          RECIPE_GROUP,
           ItemGogglesMining.create(8, new IModule[0]), "LGL", "IXI", 'L', Items.LEATHER, 'G',
           "gemDiamond", 'I', "ingotGold", 'X',
           ItemGogglesMining.create(4, new IModule[0]));
       GameRegistry.addShapedRecipe(new ResourceLocation(Global.MODID, "miningGoggles12"),
-          new ResourceLocation(Global.MODID, "Recipes"),
+          RECIPE_GROUP,
           ItemGogglesMining.create(12, new IModule[0]), "LGL", "IXI", 'L', Items.LEATHER, 'G',
           "gemEmerald", 'I', "gemDiamond", 'X',
           ItemGogglesMining.create(8, new IModule[0]));
       GameRegistry.addShapedRecipe(new ResourceLocation(Global.MODID, "miningGoggles16"),
-          new ResourceLocation(Global.MODID, "Recipes"),
+          RECIPE_GROUP,
           ItemGogglesMining.create(16, new IModule[0]), "LGL", "IXI", 'L', Items.LEATHER, 'G',
           "blockDiamond", 'I', "gemEmerald", 'X',
           ItemGogglesMining.create(12, new IModule[0]));
+      // Modules
+      GameRegistry
+          .addShapedRecipe(new ResourceLocation(Global.MODID, "moduleNightVision"), RECIPE_GROUP,
+              new ItemStack(MiningRegistry.itemModule, 1, 1), "PPP", "XDX", "PPP", 'P',
+              createPotion("minecraft:night_vision"), 'X', Items.ENDER_EYE, 'D', Blocks.END_STONE);
+      GameRegistry
+          .addShapedRecipe(new ResourceLocation(Global.MODID, "moduleNightVision2"), RECIPE_GROUP,
+              new ItemStack(MiningRegistry.itemModule, 1, 1), "PXP", "PDP", "PXP", 'P',
+              createPotion("minecraft:night_vision"), 'X', Items.ENDER_EYE, 'D', Blocks.END_STONE);
+      GameRegistry
+          .addShapedRecipe(new ResourceLocation(Global.MODID, "moduleAutoFeed"), RECIPE_GROUP,
+              new ItemStack(MiningRegistry.itemModule, 1, 0), "PPP", "XDX", "PPP", 'P',
+              new ItemStack(Items.GOLDEN_APPLE, 1, 1), 'X', Items.ENDER_EYE, 'D', Blocks.END_STONE);
+      GameRegistry
+          .addShapedRecipe(new ResourceLocation(Global.MODID, "moduleAutoFeed2"), RECIPE_GROUP,
+              new ItemStack(MiningRegistry.itemModule, 1, 0), "PXP", "PDP", "PXP", 'P',
+              new ItemStack(Items.GOLDEN_APPLE, 1, 1), 'X', Items.ENDER_EYE, 'D', Blocks.END_STONE);
+      GameRegistry.addShapedRecipe(new ResourceLocation(Global.MODID, "moduleHaste"), RECIPE_GROUP,
+          new ItemStack(MiningRegistry.itemModule, 1, 2), "PPP", "XDX", "PPP", 'P',
+          new ItemStack(Items.GOLDEN_CARROT, 1, 0), 'X', Items.ENDER_EYE, 'D', Blocks.END_STONE);
+      GameRegistry.addShapedRecipe(new ResourceLocation(Global.MODID, "moduleHaste2"), RECIPE_GROUP,
+          new ItemStack(MiningRegistry.itemModule, 1, 2), "PXP", "PDP", "PXP", 'P',
+          new ItemStack(Items.GOLDEN_CARROT, 1, 0), 'X', Items.ENDER_EYE, 'D', Blocks.END_STONE);
+      GameRegistry.addShapedRecipe(new ResourceLocation(Global.MODID, "moduleSpeed"), RECIPE_GROUP,
+          new ItemStack(MiningRegistry.itemModule, 1, 3), "PPP", "XDX", "PPP", 'P',
+          new ItemStack(Items.CAKE, 1, 0), 'X', Items.ENDER_EYE, 'D', Blocks.END_STONE);
+      GameRegistry.addShapedRecipe(new ResourceLocation(Global.MODID, "moduleSpeed2"), RECIPE_GROUP,
+          new ItemStack(MiningRegistry.itemModule, 1, 3), "PXP", "PDP", "PXP", 'P',
+          new ItemStack(Items.CAKE, 1, 0), 'X', Items.ENDER_EYE, 'D', Blocks.END_STONE);
+      GameRegistry
+          .addShapedRecipe(new ResourceLocation(Global.MODID, "moduleResistance"), RECIPE_GROUP,
+              new ItemStack(MiningRegistry.itemModule, 1, 4), "PPP", "XDX", "PPP", 'P',
+              new ItemStack(Items.SHIELD, 1,
+                  OreDictionary.WILDCARD_VALUE), 'X', Items.ENDER_EYE, 'D', Blocks.END_STONE);
+      GameRegistry
+          .addShapedRecipe(new ResourceLocation(Global.MODID, "moduleResistance2"), RECIPE_GROUP,
+              new ItemStack(MiningRegistry.itemModule, 1, 4), "PXP", "PDP", "PXP", 'P',
+              new ItemStack(Items.SHIELD, 1,
+                  OreDictionary.WILDCARD_VALUE), 'X', Items.ENDER_EYE, 'D', Blocks.END_STONE);
+      GameRegistry
+          .addShapedRecipe(new ResourceLocation(Global.MODID, "moduleWaterBreathing"), RECIPE_GROUP,
+              new ItemStack(MiningRegistry.itemModule, 1, 5), "PPP", "XDX", "PPP", 'P',
+              createPotion("minecraft:water_breathing"), 'X', Items.ENDER_EYE, 'D',
+              Blocks.END_STONE);
+      GameRegistry.addShapedRecipe(new ResourceLocation(Global.MODID, "moduleWaterBreathing2"),
+          RECIPE_GROUP, new ItemStack(MiningRegistry.itemModule, 1, 5), "PXP", "PDP", "PXP", 'P',
+          createPotion("minecraft:water_breathing"), 'X', Items.ENDER_EYE, 'D', Blocks.END_STONE);
     } catch (Exception e1) {
       e1.printStackTrace();
     }
+  }
+
+  private ItemStack createPotion(String id) {
+    NBTTagCompound nbt = new NBTTagCompound();
+    nbt.setString("Potion", id);
+    ItemStack potion = new ItemStack(Items.POTIONITEM);
+    potion.setTagCompound(nbt);
+    return potion;
   }
 
   @EventHandler
