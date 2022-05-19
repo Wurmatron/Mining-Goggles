85d6b2e7d8325df21ab1d9531ad33ffae85769b7
Wurmatron <wurmatron@gmail.com>
2022-05-19 13:06:50 -0400

    Work on Block Rendering, Updated Forge Version

--- src/main/java/io/wurmatron/mining_goggles/MiningGoggles.java
+++ src/main/java/io/wurmatron/mining_goggles/MiningGoggles.java
@@ -1,11 +1,13 @@
 package io.wurmatron.mining_goggles;
 
+import io.wurmatron.mining_goggles.client.render.RenderBlock;
 import io.wurmatron.mining_goggles.items.ItemMiningGoggles;
 import io.wurmatron.mining_goggles.items.MiningItems;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -17,21 +19,24 @@ import org.apache.logging.log4j.Logger;
 @Mod("mininggoggles")
 public class MiningGoggles {
 
-    private static final Logger LOGGER = LogManager.getLogger();
+  private static final Logger LOGGER = LogManager.getLogger();
 
-    public MiningGoggles() {
-        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-        MiningItems.register("goggles", () -> new ItemMiningGoggles(ArmorMaterial.DIAMOND, EquipmentSlotType.HEAD, new Item.Properties().maxStackSize(1).group(ItemGroup.COMBAT)));
-        MiningItems.ITEMS.register(modBus);
-    }
+  public MiningGoggles() {
+    IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+    MiningItems.register("goggles",
+        () -> new ItemMiningGoggles(ArmorMaterial.DIAMOND, EquipmentSlotType.HEAD,
+            new Item.Properties().stacksTo(1).tab(ItemGroup.TAB_COMBAT)));
+    MiningItems.ITEMS.register(modBus);
+    MinecraftForge.EVENT_BUS.register(new RenderBlock());
+  }
 
-    private void setup(final FMLCommonSetupEvent event) {
+  private void setup(final FMLCommonSetupEvent event) {
 
-    }
+  }
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
+  private void doClientStuff(final FMLClientSetupEvent event) {
 
-    }
+  }
 }
---
+++ src/main/java/io/wurmatron/mining_goggles/client/render/RenderBlock.java
@@ -0,0 +1,78 @@
+package io.wurmatron.mining_goggles.client.render;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.math.vector.Matrix4f;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import org.lwjgl.opengl.GL11;
+
+public class RenderBlock {
+
+
+  @SubscribeEvent
+  public void onRenderWorldEvent(RenderWorldLastEvent e) {
+    World world = Minecraft.getInstance().level;
+    final GameRenderer gameRenderer = Minecraft.getInstance().gameRenderer;
+    gameRenderer.resetProjectionMatrix(e.getProjectionMatrix());
+    PlayerEntity player = Minecraft.getInstance().player;
+    for (int x = -8; x < 8; x++) {
+      for (int z = -8; z < 8; z++) {
+        for (int y = 0; y < 255; y++) {
+          BlockPos pos = new BlockPos(player.getX() + x, y, player.getZ() + z);
+          BlockState state = world.getBlockState(pos);
+          if (state.is(Blocks.IRON_ORE)) {
+            drawBoundingBoxAtBlockPos(e.getMatrixStack(),
+                state.getShape(world, pos, ISelectionContext.of(player)).bounds(), 1.0F,
+                0.0F, 0.0F, 1.0F, pos);
+          }
+        }
+      }
+    }
+  }
+
+  public void drawBoundingBoxAtBlockPos(MatrixStack matrixStackIn, AxisAlignedBB aabbIn,
+      float red, float green, float blue, float alpha, BlockPos pos) {
+    Vector3d cam = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition();
+    double camX = cam.x(), camY = cam.y(), camZ = cam.z();
+    matrixStackIn.pushPose();
+    GL11.glDisable(GL11.GL_DEPTH_TEST);
+    drawShapeOutline(matrixStackIn, VoxelShapes.create(aabbIn), pos.getX() - camX,
+        pos.getY() - camY, pos.getZ() - camZ, red, green, blue, alpha);
+    GL11.glEnable(GL11.GL_DEPTH_TEST);
+    matrixStackIn.popPose();
+  }
+
+  private void drawShapeOutline(MatrixStack matrixStack, VoxelShape voxelShape,
+      double originX, double originY, double originZ, float red, float green, float blue,
+      float alpha) {
+    Matrix4f matrix4f = matrixStack.last().pose();
+    IRenderTypeBuffer.Impl renderTypeBuffer = Minecraft.getInstance().renderBuffers()
+        .bufferSource();
+    IVertexBuilder bufferIn = renderTypeBuffer.getBuffer(RenderType.lines());
+    voxelShape.forAllEdges((xMin, yMin, zMin, xMax, yMax, zMax) -> {
+      bufferIn.vertex(matrix4f, (float) (xMin + originX), (float) (yMin + originY),
+          (float) (zMin + originZ)).color(red, green, blue, alpha).endVertex();
+      bufferIn.vertex(matrix4f, (float) (xMax + originX), (float) (yMax + originY),
+          (float) (zMax + originZ)).color(red, green, blue, alpha).endVertex();
+    });
+
+    renderTypeBuffer.endBatch(RenderType.lines());
+  }
+
+
+}
